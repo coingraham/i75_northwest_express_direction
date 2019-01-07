f015ac19d49212b4e9c5375a152940af29e358fd
Coin Graham <cgraham@2ndwatch.com>
2019-01-07 08:58:49 -0500

    updating code to take time zone into account.

--- i75_northwest_express_direction.py
+++ i75_northwest_express_direction.py
@@ -1,10 +1,17 @@
-from datetime import date, datetime, time
+from datetime import datetime, time
+import pytz
+import json
+
+# Get the UTC Datetime object and converting to EST
+datetime_utc = datetime.utcnow()
+datetime_utc = datetime_utc.replace(tzinfo=pytz.UTC)
+datetime_est = datetime_utc.astimezone(pytz.timezone("US/Eastern"))
 
 # Set the day
-day = datetime.weekday(datetime.today())
+day = datetime.weekday(datetime_est)
 
 # Set the time
-time_now = datetime.today().time()
+time_now = datetime_est.time()
 
 # Set the normal operation schedule
 normal_schedule = {
@@ -71,13 +78,13 @@ normal_schedule = {
         "Changes": {
             time(11, 30): "Reverse to Northbound",
             time(13, 00): "Open to Northbound",
-            time(23, 00): "Reverse to Southbound"
+            time(23, 30): "Reverse to Southbound"
         }
     },
 }
 
 
-def get_direction():
+def get_direction(event, context):
     print("Today is {}\n".format(normal_schedule[day]["Day"]))
 
     start_for_the_day = normal_schedule[day]["Start"]
@@ -95,9 +102,16 @@ def get_direction():
                 option_in_force = start_for_the_day
 
     print("Current Status is: {}\n".format(option_in_force))
-    return "Current Status is: {}\n".format(option_in_force)
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps("Current Status is: {}".format(option_in_force))
+    }
+
+    return response
+
+    # return "Current Status is: {}".format(option_in_force)
 
 
-
-
-
+if __name__ == '__main__':
+    get_direction("", "")
